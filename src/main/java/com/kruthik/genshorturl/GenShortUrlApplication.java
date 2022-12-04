cf8690edf7a4c31e6609f2582a7659abd423a9a0
h m kruthik <kruthikhm958@gmail.com>
2022-12-04 13:18:13 +0530

    first commit

---
+++ src/main/java/com/kruthik/genshorturl/GenShortUrlApplication.java
@@ -0,0 +1,13 @@
+package com.kruthik.genshorturl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenShortUrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenShortUrlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kruthik/genshorturl/GenShortUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kruthik.genshorturl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenShortUrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
